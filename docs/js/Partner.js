20852b6c2fbf6266a8c37172047b6f10d19b9972
willy0077 <willy633526@gmail.com>
2020-02-15 12:48:07 +0800

    sponser complete

--- docs/js/Partner.js
+++ docs/js/Partner.js
@@ -1,1 +1,25 @@
-console.log("Partner");
+// console.log("Partner");
+
+new Vue({
+    el: "#app",
+    data: function () {
+        return {
+            SponsorsInfo: "",
+            // Sponsor: {
+            //     Name: "",
+            //     Look: "",
+            // },
+        }
+    },
+    created() {
+        axios.get('../asset/json/Sponsor.json').then(val => {
+            var vm = this;
+            vm.SponsorsInfo = val.data;
+            // this.Sponsor.Name = this.SponsorsInfo[0].Name;
+            // this.Sponsor.Look = this.SponsorsInfo[0].Look;
+        })
+    },
+    methods: {
+
+    }
+})
